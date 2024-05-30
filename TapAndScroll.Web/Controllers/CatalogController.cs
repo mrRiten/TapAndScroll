9a83fd65f7d5c8fd77858067f79ed4133ec08f9f
mrRiten <vitalik.chu@mail.ru>
2024-05-30 21:19:40 +0300

    Merge

---
+++ TapAndScroll.Web/Controllers/CatalogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TapAndScroll.Web.Controllers
+{
+    public class CatalogController : Controller
+    {
+        [HttpGet("Catalog/Index/{id?}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
